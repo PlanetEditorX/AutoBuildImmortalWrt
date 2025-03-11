7cbfe073b520557df4cf44f4667a0c73ff3de40c
CoderX <85693093+PlanetEditorX@users.noreply.github.com>
2025-03-11 21:17:17 +0800

    add docker-build

--- x86-64/24.10/docker-build.sh
+++ x86-64/24.10/docker-build.sh
@@ -8,9 +8,12 @@ echo "$(date '+%Y-%m-%d %H:%M:%S') - 安装插件..."
 # 定义所需安装的包列表 下列插件你都可以自行删减
 opkg update
 opkg install curl luci-i18n-diskman-zh-cn luci-i18n-firewall-zh-cn luci-app-argon-config luci-i18n-argon-config-zh-cn luci-i18n-package-manager-zh-cn luci-i18n-ttyd-zh-cn luci-i18n-passwall-zh-cn luci-app-openclash luci-i18n-homeproxy-zh-cn openssh-sftp-server fdisk script-utils luci-i18n-samba4-zh-cn
+echo "$(date '+%Y-%m-%d %H:%M:%S') - 插件安装完成"
 
-# 构建镜像
-echo "$(date '+%Y-%m-%d %H:%M:%S') - Building image with the following packages:"
-echo "$PACKAGES"
+echo "$(date '+%Y-%m-%d %H:%M:%S') - 网络初始化"
+chmod +x 99-custom.sh
+bash ./99-custom.sh
+echo "$(date '+%Y-%m-%d %H:%M:%S') - 网络初始化完成"
 
-echo "$(date '+%Y-%m-%d %H:%M:%S') - Build completed successfully."
+echo "$(date '+%Y-%m-%d %H:%M:%S') - 创建文件夹"
+mkdir -p /tmp/log /var/lock
